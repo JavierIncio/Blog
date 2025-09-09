3dd0e12e3ed196e810994357a08cb467cf3d9444
Javier Incio <javierincio.dev@gmail.com>
2025-09-09 18:47:49 +0200

    Ajuste de estilos e inicio de la sección para publicar

--- index.js
+++ index.js
@@ -12,4 +12,12 @@ APP.get("/", (req, res) => {
   res.render("index");
 });
 
+APP.get("/publish", (req, res) => {
+  res.render("publish", {
+    nombreImagen: "espinacas-con-pasas-y-pinones",
+    tituloReceta: "Espinacas con pasas y piñones"
+  });
+});
+
+
 APP.listen(PORT, () => { console.log(`Listening to port ${PORT}.`) });
